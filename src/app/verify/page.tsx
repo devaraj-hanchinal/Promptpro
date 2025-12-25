defd481151a969db9f80543beccf5beebeed7f56
devaraj-hanchinal <devarajfeb@gmail.com>
2025-12-25 09:05:34 +0530

    Update page.tsx

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,26 +1,13 @@
 "use client";
 
-import { account } from "@/lib/appwrite";
-import { useEffect, useState } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
+import { Suspense } from "react";
+import VerifyPageContent from "./verifyContent";
 
-export default function VerifyEmailPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const [status, setStatus] = useState("Verifying your email...");
-
-  useEffect(() => {
-    const userId = searchParams.get("userId");
-    const secret = searchParams.get("secret");
-
-    if (userId && secret) {
-      account.updateVerification(userId, secret)
-        .then(() => setStatus("Email verified! Redirecting to login..."))
-        .then(() => setTimeout(() => router.push("/auth/login"), 2000))
-        .catch(() => setStatus("Invalid or expired link."));
-    }
-  }, []);
-
-  return <p className="text-center mt-20">{status}</p>;
+export default function VerifyPage() {
+  return (
+    <Suspense fallback={<p className="text-center mt-10">Loading verification...</p>}>
+      <VerifyPageContent />
+    </Suspense>
+  );
 }
 
